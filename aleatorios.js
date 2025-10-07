a87023c0d2051c3746ee8eb3270e47b7852a0c7d
Moises Baez <supercapri71@gmail.com>
2025-10-06 22:08:02 -0500

    Se agrega funcion que muestra resultados

--- aleatorios.js
+++ aleatorios.js
@@ -1,25 +1,33 @@
-let numero, nota, nota1;
+let numero, nota, arregloNumeros;
 let aleatorios = [];
 
 generarAleatorios = function () {
      nota = recuperarInt("txtNota");
      if (nota > 4 && nota < 21) {
-
-          console.log("=== == = == ===");
+          console.log("=== == =", nota, " == ===");
           for (let i = 0; i < nota; i++) {
-               console.log(nota, i + 1);
+               console.log(i + 1);
           }
-
-          numero = obtieneAleatorio(x);
-          console.log(numero);
-          aleatorios.push(nota1);
+          numero = obtieneAleatorio();
+          aleatorios.push(numero);
+          arregloNumeros = aleatorios.length;
+          muestraResultado(arregloNumeros);
      } else {
           alert("Valor no permitido     el valor ingresado debe estar en el rango 5-20");
      }
 }
 
-
 obtieneAleatorio = function () {
      numero = parseInt(Math.random() * 100 + 1);
      mostrarTexto("emimi", numero);
+     return numero;
+}
+
+muestraResultado = function (arregloNumeros) {
+     console.log("*** ** * ** ***");
+
+     for (let i = 0; i < arregloNumeros; i++) {
+          console.log(i + 1,aleatorios[i]) ;
+     }
+     console.log("*** ** * ** ***");
 }
